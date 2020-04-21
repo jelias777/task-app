8fad12c4fdc78f83154a625ba64ccdb00711f6bd
Jorge Elias <jorgea.eliasl7@gmail.com>
2020-04-21 13:39:28 -0500

    feat(connect) Connect to DB and first insert

--- mongodb.js
+++ mongodb.js
@@ -1,3 +1,4 @@
+//mongodb npm library
 const mongodb = require('mongodb')
 
 //give access to connect to the DB
@@ -6,11 +7,19 @@ const MongoClient = mongodb.MongoClient
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
+//connect to server
 MongoClient.connect( connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, ( error, client ) => {
 
     if(error) {
         console.log('Unable to connect to DB')
     }
 
-    console.log('Connected Correctly')
+    //get the connection to specific DB
+    const db = client.db(databaseName)
+
+    //insert
+    db.collection('users').insertOne({
+        name: 'Jorge',
+        age: 26
+    })
 })
