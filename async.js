86daeeba89dc86415109fe476ae23ed54fb94a65
Jorge Elias <jorgea.eliasl7@gmail.com>
2020-04-27 20:39:46 -0500

    feat(async/await) Async/Await example

--- async.js
+++ async.js
@@ -1,7 +1,20 @@
+const add = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a+b)
+        }, 2000)
+    })
+}
+
 //async functions always return a promise
 const doWork = async () => {
-    //throw new Error('Something was wrong')
-    return "Hello World"
+    const sum = await add(1,2)
+
+    const sum2 = await add(sum, 50)
+
+    const sum3 = await add(sum2, 3)
+
+    return sum3
 }
 
 doWork().then((result) => {
