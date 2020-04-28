27aa05e82b5db79e3bbbfaa6a7dd3b4c1b209fc0
Jorge Elias <jorgea.eliasl7@gmail.com>
2020-04-27 20:42:44 -0500

    feat(async/await) Async/Await reject example

--- async.js
+++ async.js
@@ -1,6 +1,11 @@
 const add = (a, b) => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
+
+            if (a < 0 || b < 0) {
+                return reject('Numbers must be non-negative')
+            }
+
             resolve(a+b)
         }, 2000)
     })
@@ -12,7 +17,7 @@ const doWork = async () => {
 
     const sum2 = await add(sum, 50)
 
-    const sum3 = await add(sum2, 3)
+    const sum3 = await add(sum2, -3)
 
     return sum3
 }
