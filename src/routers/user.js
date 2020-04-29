c49e52c81a20503b0ac77bb0ceb6708d95c251e1
Jorge Elias <jorgea.eliasl7@gmail.com>
2020-04-29 13:40:56 -0500

    feat(logout) Logout from all sessions

--- src/routers/user.js
+++ src/routers/user.js
@@ -45,6 +45,18 @@ router.post('/users/logout', auth, async (req, res) => {
     }
 })
 
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try {
+
+        req.user.tokens = []
+        await req.user.save()
+        res.send()        
+
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
 router.get('/users', auth,  async (req, res) => {
     res.send(req.user)
 })
