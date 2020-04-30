0a65158ccf814c5bcaf6741cd9887477ab6d011a
Jorge Elias <jorgea.eliasl7@gmail.com>
2020-04-29 21:02:46 -0500

    feat(toJSON) Hiding data mode 2 overwriting toJSON

--- src/models/user.js
+++ src/models/user.js
@@ -49,8 +49,7 @@ const userSchema = new mongoose.Schema({
     }]
 })
 
-userSchema.methods.getPublicProfile = function () {
-    console.log('this:', this)
+userSchema.methods.toJSON = function () {
     const user = this
     const userObject = user.toObject()
 
